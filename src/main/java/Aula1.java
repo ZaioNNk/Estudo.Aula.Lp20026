fb5ee017fb3a7f71bb49048fe0a2cc32baa97e55
ZaioNNk <zaion460@gmail.com>
2026-09-08 22:56:38 -0300

    Calculo imc aula 1

--- src/main/java/Aula1.java
+++ src/main/java/Aula1.java
@@ -1,6 +1,15 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Aula1 {
     static void main() {
-        System.out.println("Joao viado");
+
+        String pesoString = JOptionPane.showInputDialog("Insira o peso: ");
+        double peso = Double.parseDouble(pesoString);
+        String alturaString = JOptionPane.showInputDialog("Insira a altura: ");
+        double altura = Double.parseDouble(alturaString);
+        double imc = peso / (altura * altura);
+        JOptionPane.showMessageDialog(null, imc);
 
     }
 }
